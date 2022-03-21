c0ce6fb08170d8b70d243f0e7b62088732eebbe1
Shari Pavoni <sharidkpavoni@gmail.com>
2022-03-21 20:56:19 +0200

    Basic socket.io server and client implementation

---
+++ src/server/hub/main.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import path from "path";
+
+const app = express();
+const server = http.createServer(app);
+
+/* Creating a socket.io server instance */
+const io = new Server(server, {
+    cors: { origin: "*" },
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+
+io.on('connection', (socket) => {
+    console.log(`SERVER: User connected ${socket.id}`);
+})
+
+
+server.listen(3001, "0.0.0.0", () => {
+    console.log("Listening to port: 3001");
+});
